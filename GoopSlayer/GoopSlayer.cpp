8d6a1e99eb5bc1d913ae767cd10521ada28f2c2f
WedrownyElite <chris.sliwinski@gmail.com>
2023-09-26 15:00:43 -0500

    Ooga

--- GoopSlayer/GoopSlayer.cpp
+++ GoopSlayer/GoopSlayer.cpp
@@ -6,11 +6,14 @@
 
 class GoopSlayer : public olc::PixelGameEngine {
 public:
-	//Sprites / pngs
+	//Sprites
 	std::unique_ptr<olc::Sprite> GoopRight;	
 	std::unique_ptr<olc::Sprite> Grass;
+	std::unique_ptr<olc::Sprite> ArcherRight;
+	//Decals
 	olc::Decal* GrassDecal;
 	olc::Decal* GoopRightDecal;
+	olc::Decal* ArcherRightDecal;
 
 	float GoopX = 0;
 	float GoopY = 50;
@@ -30,21 +33,29 @@ public:
 	void MoveGoop() {
 		GoopX += 1;
 		DrawDecal({ (float)GoopX, (float)GoopY }, GoopRightDecal, { (float)2, (float)2 });
+		if (GoopX == 960) {
+			GoopX = 0;
+		}
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override {
 		Clear(olc::BLACK);
 		DrawGrass();
 		MoveGoop();
+		DrawDecal({ (float)448, (float)238 }, ArcherRightDecal, { (float)2, (float)2 });
 		return true;
 	}
 
 private:
 	bool OnUserCreate() override {
-		Grass = std::make_unique<olc::Sprite>("./Sprites/Fuck.png");
+		//Sprites
+		ArcherRight = std::make_unique<olc::Sprite>("./Sprites/ArcherRight.png");
+		Grass = std::make_unique<olc::Sprite>("./Sprites/Grass.png");
 		GoopRight = std::make_unique<olc::Sprite>("./Sprites/GoopRight.png");
+		//Decals
 		GrassDecal = new olc::Decal(Grass.get());
 		GoopRightDecal = new olc::Decal(GoopRight.get());
+		ArcherRightDecal = new olc::Decal(ArcherRight.get());
 		return true;
 	}
 };
