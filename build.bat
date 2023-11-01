b848de29dda7730176bc2e5e99e97944a5e30625
Moros1138 <moros1138@gmail.com>
2023-10-31 20:06:32 -0400

    add emscripten build script

---
+++ build.bat
@@ -0,0 +1,3 @@
+cd GoopSlayer
+em++ -std=c++17 -O2 -sALLOW_MEMORY_GROWTH=1 -sMAX_WEBGL_VERSION=2 -sMIN_WEBGL_VERSION=2 -sUSE_LIBPNG=1 --preload-file ./Sprites@Sprites GoopSlayer.cpp -o ../GoopSlayer.html
+cd ..
